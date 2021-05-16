0e5ad3445c4116c635343b7a3fb60b9e7fd81bf2
trypolis <60036684+TyGillespie@users.noreply.github.com>
2021-05-16 14:13:07 -0600

    Initial code and structure.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+export var ttsVoice;
+export var ttsRate = 1;
+
+document.addEventListener('DOMContentLoaded', setup);
+async function setup() {
+  document.getElementById('app').focus();
+  // Call your main function here.
+  console.log('Success!');
+}
